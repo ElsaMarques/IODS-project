e084204aab00414d85b17b93d94b967041b20fed
ElsaMarques <elsaoliveiramarques@gmail.com>
2017-11-22 20:23:36 +0200

    chapter 3 changes

--- data/create_alc.R
+++ data/create_alc.R
@@ -1,10 +1,13 @@
-#Elsa MArques R Studio exercice 3 
+#Elsa Marques - R Studio exercice 3 
 
 #Load libraries that will be used further on
 library(dplyr)
 library(ggplot2)
 library(GGally)
 library(tidyr)
+
+#Set the working directory
+setwd("~/Documents/GitHub/IODS-project")
 
 #Load the data stored locally on the project folder 
 read("student-mat.csv", sep = ";")
@@ -39,7 +42,8 @@ glimpse(math_por)
 str(math_por)
 dim(math_por)
 
-#EDIT FURTHER FROM HERE ONWARDS 
+#Use the joined data to create a new data frame
+alc <- select(math_por, one_of(join_by))
 
 #View the columns from the datasets which were not used for joining the data
 notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
@@ -68,134 +72,23 @@ for(column_name in notjoined_columns) {
 # glimpse at the new combined data
 glimpse(alc)
 
-
-# define a new column alc_use by combining weekday and weekend alcohol use
+# average weekday and weekend alcohol consumption to create a new column 'alc_use'
 alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
 
-# initialize a plot of alcohol use
-g1 <- ggplot(data = alc, aes(x = alc_use, fill = sex))
-
-# define the plot as a bar plot and draw it
-g1 + geom_bar()
-
-# define a new logical column 'high_use'
+# 'alc_use' used to create a new logical column 'high_use' (students with 'alc_use' > 2)
 alc <- mutate(alc, high_use = alc_use > 2)
 
-# initialize a plot of 'high_use'
-g2 <- ggplot(data = alc, aes(x = high_use, fill = sex))
-
-# draw a bar plot of high_use by sex
-g2 + facet_wrap("sex")
-
-
-# use gather() to gather columns into key-value pairs and then glimpse() at the resulting data
-gather(alc) %>% glimpse
-
-# draw a bar plot of each variable
-gather(alc) %>% ggplot(aes(value)) + facet_wrap("key", scales = "free") + geom_bar()
+# gather columns into key-value pairs and then glimpse the resulting data
+gather(alc) %>% glimpse()
 
 # produce summary statistics by group
 alc %>% group_by(sex, high_use) %>% summarise(count = n(), mean_grade = mean(G3))
 
+#save the combined data as .CSV
+write.csv(alc, file = "student-combined.csv" )
 
-
-# initialize a plot of high_use and G3
-g1 <- ggplot(alc, aes(x = high_use, y = G3, col = sex))
-
-# define the plot as a boxplot and draw it
-g1 + geom_boxplot() + ylab("grade")
-
-# initialise a plot of high_use and absences
-g2 <- ggplot(alc, aes(x = high_use, y = absences, col = sex))
-
-# define the plot as a boxplot and draw it
-g2 + geom_boxplot() + ggtitle("Student absences by alcohol consumption and sex")
-
-
-
-# find the model with glm()
-m <- glm(high_use ~ failures + absences + sex, data = alc, family = "binomial")
-
-# print out a summary of the model
-summary(m)
-
-# print out the coefficients of the model
-coef(m)
-
-
-# find the model with glm()
-m <- glm(high_use ~ failures + absences + sex, data = alc, family = "binomial")
-
-# compute odds ratios (OR)
-OR <- coef(m) %>% exp
-
-# compute confidence intervals (CI)
-CI <- confint(m) %>% exp
-
-# print out the odds ratios with their confidence intervals
-cbind(OR, CI)
-
-
-# fit the model
-m <- glm(high_use ~ failures + absences + sex, data = alc, family = "binomial")
-
-# predict() the probability of high_use
-probabilities <- predict(m, type = "response")
-
-# add the predicted probabilities to 'alc'
-alc <- mutate(alc, probability = probabilities)
-
-# use the probabilities to make a prediction of high_use
-alc <- mutate(alc, prediction = probability > 0.5)
-
-# see the last ten original classes, predicted probabilities, and class predictions
-select(alc, failures, absences, sex, high_use, probability, prediction) %>% head(10)
-
-# tabulate the target variable versus the predictions
-table(high_use = alc$high_use, prediction = alc$probability > 0.5)
-
-
-
-# initialize a plot of 'high_use' versus 'probability' in 'alc'
-g <- ggplot(alc, aes(x = probability, y = high_use, col = prediction))
-
-# define the geom as points and draw the plot
-g + geom_point()
-
-# tabulate the target variable versus the predictions
-table(high_use = alc$high_use, prediction = alc$prediction) %>%
-  prop.table() %>% addmargins () 
-
-
-# the logistic regression model m and dataset alc with predictions are available
-
-# define a loss function (mean prediction error)
-loss_func <- function(class, prob) {
-  n_wrong <- abs(class - prob) > 0.5
-  mean(n_wrong)
-}
-
-# call loss_func to compute the average number of wrong predictions in the (training) data
-loss_func(class = alc$high_use, prob = alc$probability)
-
-
-# the logistic regression model m and dataset alc (with predictions) are available
-
-# define a loss function (average prediction error)
-loss_func <- function(class, prob) {
-  n_wrong <- abs(class - prob) > 0.5
-  mean(n_wrong)
-}
-
-# compute the average number of wrong predictions in the (training) data
-loss_func(class = alc$high_use, prob = alc$probability)
-
-# K-fold cross-validation
-library(boot)
-cv <- cv.glm(data = alc, cost = loss_func, glmfit = m, K = 10)
-
-# average number of wrong predictions in the cross validation
-cv$delta[1]
+#check you can read the data back
+read.csv("student-combined.csv")
 
 
 
